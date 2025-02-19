ed1ffa09fe95183f599c234f21dc05ba033ebc69
Victor Trumpel <victortrumpel@MacBook-Pro-Victor.local>
2025-02-19 23:09:33 +0300

    feat: add random spawn of orks

--- src/features/mounters/EnemiesSceneMounter.ts
+++ src/features/mounters/EnemiesSceneMounter.ts
@@ -19,7 +19,18 @@ export class EnemiesSceneMounter {
 
   private handleCreateEnemy = () => {
     if (this.enemiesCount >= MAX_ENEMY_COUNT) return
-    this.enemiesGroup.add(this.createEnemySprite(1400, 1300))
+    const spawnSide = {
+      0: { x: this.getRandomEnemyPosition(0, 2000), y: this.getRandomEnemyPosition(-20, 0) },
+      1: { x: this.getRandomEnemyPosition(2000, 2050), y: this.getRandomEnemyPosition(0, 2000) },
+      2: { x: this.getRandomEnemyPosition(0, 2000), y: this.getRandomEnemyPosition(2000, 2050) },
+      3: { x: this.getRandomEnemyPosition(-20, 0), y: this.getRandomEnemyPosition(0, 2000) },
+    }
+
+    const randomSide = this.getRandomInt(0, 3)
+
+    const { x, y } = spawnSide[randomSide as 0 | 1 | 2 | 3]
+
+    this.enemiesGroup.add(this.createEnemySprite(x, y))
   }
 
   private get enemiesCount() {
@@ -34,9 +45,19 @@ export class EnemiesSceneMounter {
 
   private create() {
     this.scene.time.addEvent({
-      delay: 5000,
+      delay: 8000,
       callback: this.handleCreateEnemy,
       loop: true,
     })
+  }
+
+  private getRandomInt(min: number, max: number) {
+    const minInt = Math.ceil(min)
+    const maxInt = Math.floor(max)
+    return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt
+  }
+
+  private getRandomEnemyPosition(min: number, max: number) {
+    return Math.random() * (max - min) + min
   }
 }
